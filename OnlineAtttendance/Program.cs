f682978aeef68a5d78deac80ee43d8e6672140c0
Sagha <Sagha@LAPTOP-AUS1OPRL>
2019-12-08 23:38:01 +0400

    Add project files.

---
+++ OnlineAtttendance/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineAtttendance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
